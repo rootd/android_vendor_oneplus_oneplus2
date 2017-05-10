eff0ae681735b1068c7422a69805350ab218b9bf
Alexander Meiler <demonmeetsflair@yahoo.com>
2017-05-10 15:25:38 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -158,32 +158,6 @@ LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE        := libbt-vendor
-LOCAL_MODULE_CLASS  := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_STRIP_MODULE  := false
-LOCAL_MULTILIB      := 64
-LOCAL_MODULE_OWNER  := qcom
-LOCAL_MODULE_TAGS   := optional
-LOCAL_SRC_FILES     := vendor/lib64/libbt-vendor.so
-LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/system/vendor/lib64
-LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE        := libbt-vendor
-LOCAL_MODULE_CLASS  := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_STRIP_MODULE  := false
-LOCAL_MULTILIB      := 32
-LOCAL_MODULE_OWNER  := qcom
-LOCAL_MODULE_TAGS   := optional
-LOCAL_SRC_FILES     := vendor/lib/libbt-vendor.so
-LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/system/vendor/lib
-LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE        := FingerprintServiceExtension
 LOCAL_MODULE_OWNER  := qcom
 LOCAL_MODULE_TAGS   := optional
